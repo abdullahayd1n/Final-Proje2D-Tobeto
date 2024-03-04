528d04e6a86b9d7a71a5bc475445f471f48ba034
Alican <alicanyesilay1@gmail.com>
2024-03-04 04:47:10 +0300

    ss

--- My project/Assets/AlicanSahne/Scripts/Lucas Scripts/LucasHealth.cs
+++ My project/Assets/AlicanSahne/Scripts/Lucas Scripts/LucasHealth.cs
@@ -66,7 +66,7 @@ public class LucasHealth : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.layer == LayerMask.NameToLayer("Health"))
+        if (other.gameObject.layer == LayerMask.NameToLayer("TakeHeal"))
         {
             // Saðlýk objesini yok et
             Destroy(other.gameObject);
