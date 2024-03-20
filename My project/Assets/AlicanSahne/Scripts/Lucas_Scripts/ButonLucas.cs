9f420d56c68eaef9283a3fde49e8e1d85d07b61b
Alican <alicanyesilay1@gmail.com>
2024-03-21 01:20:54 +0300

    Menü ıvır zıvır

--- My project/Assets/AlicanSahne/Scripts/Lucas_Scripts/ButonLucas.cs
+++ My project/Assets/AlicanSahne/Scripts/Lucas_Scripts/ButonLucas.cs
@@ -14,18 +14,26 @@ public class ButonLucas : MonoBehaviour
     private bool isFacingRight = true;
     private bool isMoving = false;
 
+    // Puan metni
     public Text score;
+    // Puan deðeri
     private int scoreValue = 0;
 
+    // Yadigar puan metni
     public Text yadigarScore;
+    // Yadigar puan deðeri
     private int yadigarValue = 0;
+
+    // Oyun baþlangýcýnda çalýþacak metod
     private void Awake()
     {
+        // Yadigar puaný kaydedilmiþse yükle ve ekrana yaz
         if (PlayerPrefs.HasKey("ButonLucas_Yadigar"))
         {
             yadigarValue = PlayerPrefs.GetInt("ButonLucas_Yadigar");
             yadigarScore.text = " " + yadigarValue;
         }
+        // Normal puan kaydedilmiþse yükle ve ekrana yaz
         if (PlayerPrefs.HasKey("ButonLucas_Coins"))
         {
             scoreValue = PlayerPrefs.GetInt("ButonLucas_Coins");
@@ -41,7 +49,7 @@ public class ButonLucas : MonoBehaviour
             Jump();
         }
 
-        UpdateAnimation(); // UpdateAnimation yöntemini çaðýr
+        UpdateAnimation();
     }
 
     public void MoveLeft()
@@ -49,10 +57,12 @@ public class ButonLucas : MonoBehaviour
         Move(-1f);
     }
 
+
     public void MoveRight()
     {
         Move(1f);
     }
+
 
     public void StopMoving()
     {
@@ -66,6 +76,7 @@ public class ButonLucas : MonoBehaviour
         float horizontal = direction * speed;
         rb.velocity = new Vector2(horizontal, rb.velocity.y);
 
+        // Karakterin yönünü çevir
         if (!isFacingRight && direction > 0f)
         {
             Flip();
@@ -75,6 +86,7 @@ public class ButonLucas : MonoBehaviour
             Flip();
         }
 
+        // Hareket ediyorsa ve daha önce hareket etmiyorsa animasyonu baþlat
         if (!isMoving)
         {
             isMoving = true;
@@ -101,37 +113,48 @@ public class ButonLucas : MonoBehaviour
         transform.Rotate(new Vector3(0, 180, 0));
         isFacingRight = !isFacingRight;
     }
-
     private void UpdateAnimation()
     {
         anim.SetBool("jump", !IsGrounded());
         anim.SetBool("walk", isMoving && IsGrounded());
     }
 
+    // Tetikleyici ile etkileþime girildiðinde çalýþacak metod
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // Yadigar objesi ile etkileþime girildiyse
         if (collision.gameObject.tag == "Yadigar")
         {
+            // Yadigar objesini devre dýþý býrak
             collision.gameObject.SetActive(false);
+            // Yadigar puanýný artýr
             yadigarValue += 1;
-            yadigarScore.text = " " + yadigarValue; // Puaný güncelle
-            PlayerPrefs.SetInt("ButonLucas_Yadigar", yadigarValue); // Yadigar coin puanýný sakla
+            // Yadigar puanýný ekrana yaz
+            yadigarScore.text = " " + yadigarValue;
+            // Yadigar puanýný kaydet
+            PlayerPrefs.SetInt("ButonLucas_Yadigar", yadigarValue);
         }
+        // Normal para objesi ile etkileþime girildiyse
         else if (collision.gameObject.tag == "Coins")
         {
+            // Para objesini devre dýþý býrak
             collision.gameObject.SetActive(false);
+            // Puaný artýr
             scoreValue += 1;
-            score.text = " " + scoreValue; // Puaný güncelle
-            PlayerPrefs.SetInt("ButonLucas_Coins", scoreValue); // Normal coin puanýný sakla
+            // Puaný ekrana yaz
+            score.text = " " + scoreValue;
+            // Puaný kaydet
+            PlayerPrefs.SetInt("ButonLucas_Coins", scoreValue);
         }
     }
 
-
-
+    // Puanlarý güncelleyen metod
     void SetScore()
     {
+        // Yadigar puanýný yadigarScore metin alanýnda göstermek için deðeri ayarla
         yadigarScore.text = " " + yadigarValue;
+        // Puaný score metin alanýnda göstermek için deðeri ayarla
         score.text = " " + scoreValue;
-
     }
+
 }
--- My project/Assets/AlicanSahne/Scripts/MenuScripts/YadigarCoinScriptt.cs
+++ My project/Assets/AlicanSahne/Scripts/MenuScripts/YadigarCoinScriptt.cs
@@ -3,12 +3,13 @@ using UnityEngine.UI;
 
 public class YadigarCoinScriptt : MonoBehaviour
 {
-    public Text  yadigarText; // Altýn sayýsýný gösterecek metin alaný
+    public Text yadigarText; // Altýn sayýsýný gösterecek metin alaný
 
     void Start()
     {
+        // Oyuncunun toplam yadigar sayýsýný PlayerPrefs'ten al
         int collectedYadigar = PlayerPrefs.GetInt("ButonLucas_Yadigar", 0);
+        // Metin alanýnda toplanan yadigar sayýsýný göster
         yadigarText.text = " " + collectedYadigar.ToString();
     }
-
 }
