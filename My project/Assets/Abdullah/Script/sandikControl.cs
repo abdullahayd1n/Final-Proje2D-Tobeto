4c62f2c1ee4464ae01b813e4d8682edff6b25920
Abdullah Aydın <147834087+abdullahayd1n@users.noreply.github.com>
2024-04-05 00:30:46 +0300

    Level1Duzenlemeler

--- My project/Assets/Abdullah/Script/sandikControl.cs
+++ My project/Assets/Abdullah/Script/sandikControl.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class sandikControl : MonoBehaviour
 {
     public Animator chestAnimator; // Sandýk animator bileþeni
-
     private bool isPlayerInside; // Karakterin sandýk içinde olup olmadýðýný belirten boolean
+    public GameObject button; // Açma düðmesi
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             isPlayerInside = true; // Karakter sandýk içine girdiðinde boolean deðeri true olarak ayarla
+            button.SetActive(true);
         }
     }
 
@@ -21,16 +22,27 @@ public class sandikControl : MonoBehaviour
         if (collision.CompareTag("Player"))
         {
             isPlayerInside = false; // Karakter sandýk içinden çýktýðýnda boolean deðeri false olarak ayarla
+            button.SetActive(false);
         }
     }
 
     private void Update()
     {
-        if (isPlayerInside && Input.GetKeyDown(KeyCode.E)) // E tuþuna basýldýðýnda ve karakter sandýk içindeyken
+        // E tuþuna basýldýðýnda ve karakter sandýk içindeyken
+        if (isPlayerInside && Input.GetKeyDown(KeyCode.E))
         {
-            chestAnimator.SetBool("isOpen", true); // Sandýk animasyonunun "isOpen" parametresini true yaparak açýlmasýný saðla
+            OpenChest(); // Sandýðý aç
         }
     }
-}
 
+    public void OpenChestOnClick()
+    {
+        OpenChest(); // Düðmeye týklandýðýnda sandýðý aç
+    }
+
+    private void OpenChest()
+    {
+        chestAnimator.SetBool("isOpen", true); // Sandýk animasyonunun "isOpen" parametresini true yaparak açýlmasýný saðla
+    }
+}
 
