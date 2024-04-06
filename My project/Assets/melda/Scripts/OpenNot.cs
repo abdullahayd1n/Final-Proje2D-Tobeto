adf05616b7941d2b14eba15c5d7284505018c510
meldayetkin <146190690+meldayetkin@users.noreply.github.com>
2024-04-07 01:00:29 +0300

    sesler eklendi

--- My project/Assets/melda/Scripts/OpenNot.cs
+++ My project/Assets/melda/Scripts/OpenNot.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class OpenNot : MonoBehaviour
 {
     public GameObject notObject;
+
+    public AudioSource audioSource;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player")&& notObject != null)
         {
             notObject.SetActive(true);
+            audioSource.Play();
         }
     }
     void OnTriggerExit2D(Collider2D other)
