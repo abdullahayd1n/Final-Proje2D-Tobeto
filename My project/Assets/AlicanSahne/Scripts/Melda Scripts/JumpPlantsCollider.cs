a6c417b1d3a18917b0707cf8cef9d264f5af3c49
meldayetkin <146190690+meldayetkin@users.noreply.github.com>
2024-04-10 00:15:12 +0300

    xxx

---
+++ My project/Assets/AlicanSahne/Scripts/Melda Scripts/JumpPlantsCollider.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPlantsCollider : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player") && !audioSource.isPlaying)
+        {
+            audioSource.Play();
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.CompareTag("Player") && audioSource.isPlaying)
+        {
+            audioSource.Stop();
+        }
+    }
+}
---
+++ My project/Assets/AlicanSahne/Scripts/Melda Scripts/MainMenuSoundManager.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MainMenuSoundManager : MonoBehaviour
+{
+    public static MainMenuSoundManager Instance;
+
+    public Sound[] musicSounds, sfxSound;
+    public AudioSource musicSource, sfxSoucse;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            
+        }
+        
+    }
+    private void Start()
+    {
+        PlayMusic("Theme");
+    }
+    public void PlayMusic(string name)
+    {
+        Sound s=Array.Find(musicSounds,x=>x.name==name);
+
+        if(s == null)
+        {
+            Debug.Log("Soun Not Found");
+        }
+        else
+        {
+            musicSource.clip = s.clip;
+            musicSource.Play();
+        }
+    }
+
+    public void PlaySFX(string name)
+    {
+        Sound s = Array.Find(sfxSound,x=>x.name==name);
+
+        if(s == null)
+        {
+            Debug.Log("Sound Not Found");
+        }
+        else
+        {
+            sfxSoucse.PlayOneShot(s.clip);
+        }
+    }
+
+    public void ToggleMusic()
+    {
+        musicSource.mute=!musicSource.mute;
+    }
+    public void ToggleSFX()
+    {
+        sfxSoucse.mute=!sfxSoucse.mute;
+    }
+    public void MusicVolume(float volume)
+    {
+        musicSource.volume = volume;
+    }
+    public void SFXVolume(float volume)
+    {
+        sfxSoucse.volume = volume;
+    }
+
+    public void PlayButtonSound()
+    {
+        MainMenuSoundManager.Instance.PlaySFX("Options");
+    }
+
+    public void CancelButtonSound()
+    {
+        MainMenuSoundManager.Instance.PlaySFX("Cancel");
+
+    }
+
+    public void FullScreenButtonSound()
+    {
+        MainMenuSoundManager.Instance.PlaySFX("FullScreen");
+    }
+}
