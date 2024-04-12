146224a05f7adf2c0838df5608fd7d212c11b13f
Abdullah Aydın <147834087+abdullahayd1n@users.noreply.github.com>
2024-04-12 20:16:50 +0300

    LEVEL1 CINEMACHIN CAMERALAR / KONUSMA BALONLARI

--- My project/Assets/AlicanSahne/Scripts/SoundScripts/AudioManager.cs
+++ My project/Assets/AlicanSahne/Scripts/SoundScripts/AudioManager.cs
@@ -108,14 +108,13 @@ public class AudioManager : MonoBehaviour
     {
         AudioManager.Instance.PlaySFX("PanelFullScreen");
     }
-
-
 }
-
 [System.Serializable]
 public class Sound
 {
     public string name;
     public AudioClip clip;
 }
+
+
 
