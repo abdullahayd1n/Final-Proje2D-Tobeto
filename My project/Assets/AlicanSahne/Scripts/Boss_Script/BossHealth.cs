67e82f13defcfd0acd1e41bb9e9d977d6725785e
Abdullah Aydın <147834087+abdullahayd1n@users.noreply.github.com>
2024-04-13 15:40:26 +0300

    magaraBoos

--- My project/Assets/AlicanSahne/Scripts/Boss_Script/BossHealth.cs
+++ My project/Assets/AlicanSahne/Scripts/Boss_Script/BossHealth.cs
@@ -4,22 +4,17 @@ using UnityEngine;
 
 public class BossHealth : MonoBehaviour
 {
+    public GameObject tas1;
+    public GameObject tas2;
+    public Cinemachine.CinemachineVirtualCamera PlayerCam;
+    public Cinemachine.CinemachineVirtualCamera bosCamera;
+
     public int health = 500;
     public GameObject deathEffect;
     public bool isInvulnerable = false;
     private bool isDead = false;
-    public float removeEnemy = 1.3f; // Düþmaný kaldýrma süresi
+    public float removeEnemy = 2f; // Düþmaný kaldýrma süresi
     private Animator anim;
-
-    // Ölüm sonrasý atýlacak para prefabý
-    public GameObject moneyPrefab;
-    public int moneyAmount = 21;
-
-    // Item drop prefab'larý
-    public GameObject[] itemDropPrefabs;
-
-    // Düþüþ sayacý
-    private int dropCounter = 0;
 
     void Start()
     {
@@ -33,28 +28,6 @@ public class BossHealth : MonoBehaviour
 
         health -= damage;
 
-        // Her %25'lik dilimde bir kez düþürme kontrolü
-        if (health <= 375 && dropCounter == 0)
-        {
-            DropItem();
-            dropCounter++;
-        }
-        else if (health <= 250 && dropCounter == 1)
-        {
-            DropItem();
-            dropCounter++;
-        }
-        else if (health <= 125 && dropCounter == 2)
-        {
-            DropItem();
-            dropCounter++;
-        }
-        else if (health <= 0 && dropCounter == 3)
-        {
-            DropItem();
-            dropCounter++;
-        }
-
         if (health <= 0 && !isDead)
         {
             Die();
@@ -67,20 +40,24 @@ public class BossHealth : MonoBehaviour
         anim.SetTrigger("Die");
 
         // Oyunu yavaþlatmak için Time.timeScale kullanýlýr
-        Time.timeScale = 0.1f; // Yavaþlama faktörünü ayarlayabilirsiniz
-
-        // Ölüm sonrasý paralarýn atýlmasý
-        for (int i = 0; i < moneyAmount; i++)
-        {
-            Instantiate(moneyPrefab, transform.position + new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0), Quaternion.identity);
-        }
+        Time.timeScale = 0.5f; // Yavaþlama faktörünü ayarlayabilirsiniz
 
         StartCoroutine(DisableBossAndResumeGame());
+
+        Destroy(tas1.gameObject);
+        Destroy(tas2.gameObject);
+        Invoke("SwitchCamera", 1.5f);
+
+    }
+    private void SwitchCamera()
+    {
+        PlayerCam.Priority = 20; // Ana kamerayý düþük öncelikle ac
+        bosCamera.Priority = 10; // Cinematic kamerayý yüksek öncelikle kapat
     }
 
     IEnumerator DisableBossAndResumeGame()
     {
-        yield return new WaitForSeconds(0.8f); // Animasyonun süresine göre ayarlayýn
+        yield return new WaitForSeconds(1.5f); // Animasyonun süresine göre ayarlayýn
 
         // Oyunu yeniden normal hýzda çalýþtýr
         Time.timeScale = 1f;
@@ -92,16 +69,6 @@ public class BossHealth : MonoBehaviour
         if (deathEffect != null)
         {
             Instantiate(deathEffect, transform.position, Quaternion.identity);
-        }
-    }
-
-    // Rastgele bir öðe düþürme fonksiyonu
-    private void DropItem()
-    {
-        if (itemDropPrefabs.Length > 0)
-        {
-            int randomIndex = Random.Range(0, itemDropPrefabs.Length); // Rastgele bir index seçer.
-            Instantiate(itemDropPrefabs[randomIndex], transform.position + new Vector3(0, 1, 0), Quaternion.identity);
         }
     }
 }
---
+++ My project/Assets/bosGris.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bosGris : MonoBehaviour
+{
+    public Cinemachine.CinemachineVirtualCamera PlayerCam;
+    public Cinemachine.CinemachineVirtualCamera bosCamera;
+    public Rigidbody2D rb;
+    public Rigidbody2D rb1;
+
+    private float fallDelay = 0.001f;
+
+    private bool isSwitched = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            StartCoroutine(TriggeredFall());
+        }
+
+        isSwitched = true;
+        SwitchCamera();
+    }
+
+    private void SwitchCamera()
+    {
+        PlayerCam.Priority = 10; // Ana kamerayý düþük öncelikle kapat
+        bosCamera.Priority = 20; // Cinematic kamerayý yüksek öncelikle aç
+    }
+
+    private IEnumerator TriggeredFall()
+    {
+        yield return new WaitForSeconds(fallDelay);
+        rb.bodyType = RigidbodyType2D.Dynamic;
+        rb1.bodyType = RigidbodyType2D.Dynamic;
+    }
+}
