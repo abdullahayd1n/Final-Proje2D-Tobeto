b2c3787eb970fea14484b2aec2691548be4d7626
Alican <alicanyesilay1@gmail.com>
2024-04-13 14:09:02 +0300

    Boss içi itemler ayarlandı

--- My project/Assets/AlicanSahne/Scripts/Boss_Script/BossHealth.cs
+++ My project/Assets/AlicanSahne/Scripts/Boss_Script/BossHealth.cs
@@ -11,6 +11,16 @@ public class BossHealth : MonoBehaviour
     public float removeEnemy = 1.3f; // Düþmaný kaldýrma süresi
     private Animator anim;
 
+    // Ölüm sonrasý atýlacak para prefabý
+    public GameObject moneyPrefab;
+    public int moneyAmount = 21;
+
+    // Item drop prefab'larý
+    public GameObject[] itemDropPrefabs;
+
+    // Düþüþ sayacý
+    private int dropCounter = 0;
+
     void Start()
     {
         anim = GetComponent<Animator>();
@@ -22,6 +32,28 @@ public class BossHealth : MonoBehaviour
             return;
 
         health -= damage;
+
+        // Her %25'lik dilimde bir kez düþürme kontrolü
+        if (health <= 375 && dropCounter == 0)
+        {
+            DropItem();
+            dropCounter++;
+        }
+        else if (health <= 250 && dropCounter == 1)
+        {
+            DropItem();
+            dropCounter++;
+        }
+        else if (health <= 125 && dropCounter == 2)
+        {
+            DropItem();
+            dropCounter++;
+        }
+        else if (health <= 0 && dropCounter == 3)
+        {
+            DropItem();
+            dropCounter++;
+        }
 
         if (health <= 0 && !isDead)
         {
@@ -36,6 +68,12 @@ public class BossHealth : MonoBehaviour
 
         // Oyunu yavaþlatmak için Time.timeScale kullanýlýr
         Time.timeScale = 0.1f; // Yavaþlama faktörünü ayarlayabilirsiniz
+
+        // Ölüm sonrasý paralarýn atýlmasý
+        for (int i = 0; i < moneyAmount; i++)
+        {
+            Instantiate(moneyPrefab, transform.position + new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0), Quaternion.identity);
+        }
 
         StartCoroutine(DisableBossAndResumeGame());
     }
@@ -54,6 +92,16 @@ public class BossHealth : MonoBehaviour
         if (deathEffect != null)
         {
             Instantiate(deathEffect, transform.position, Quaternion.identity);
+        }
+    }
+
+    // Rastgele bir öðe düþürme fonksiyonu
+    private void DropItem()
+    {
+        if (itemDropPrefabs.Length > 0)
+        {
+            int randomIndex = Random.Range(0, itemDropPrefabs.Length); // Rastgele bir index seçer.
+            Instantiate(itemDropPrefabs[randomIndex], transform.position + new Vector3(0, 1, 0), Quaternion.identity);
         }
     }
 }
